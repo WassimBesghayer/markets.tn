7f18731a05cd6f8c351c829fd924f61427d507c8
Wassim <wassim.besghayer@gmail.com>
2024-12-26 09:36:43 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li>Blog</li>
+          <li>Deals</li>
+          <li>Clearances</li>
+        </ul>
+      </nav>
+      <aside>
+        <ul>
+          <li>Vehicles</li>
+          <li>Home Appliances</li>
+          <li>Electronics</li>
+          <li>Real Estate</li>
+        </ul>
+      </aside>
+    </div>
+  );
+}
+
+export default App;
